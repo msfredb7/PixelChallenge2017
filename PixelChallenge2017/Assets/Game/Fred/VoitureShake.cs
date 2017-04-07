a4164810d8cbdfc7ff5b39319543a86dd8386b96
Fred Bessette <msfredb7@gmail.com>
2017-04-07 13:34:19 -0400

    Animations ! de base

---
+++ PixelChallenge2017/Assets/Game/Fred/VoitureShake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoitureShake : MonoBehaviour {
+
+    Vector3 startPos;
+    public float shake;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = startPos + new Vector3(Random.Range(-shake * 0.1f, shake * 0.1f), 0, Random.Range(-shake * 0.1f, shake * 0.1f));
+    }
+}
