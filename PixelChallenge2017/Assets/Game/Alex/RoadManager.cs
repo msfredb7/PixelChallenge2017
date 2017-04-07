a88dddfdb263fb5b667efa3cf88c30b6c0f2daef
Xeal <alexandre-blanchet94@hotmail.com>
2017-04-06 23:21:27 -0400

    Premier pitch du system devennement!

---
+++ PixelChallenge2017/Assets/Game/Alex/RoadManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : PublicSingleton<RoadManager> {
+
+    public Road currentRoad;
+
+    public float startTime;
+
+    void Update()
+    {
+        if (currentRoad == null)
+        {
+            print("Ain't going anywhere bruh");
+            return;
+        }
+
+        float currentDistance = Time.time - startTime; // 1km = 1 secondes
+
+        print("They see me rollin' ! They hatin' ...( Distance : " + currentDistance);
+
+        Stop nextStop = currentRoad.GetNextStop(currentDistance);
+        SpecialEvent nextEvent = currentRoad.GetNextSpecialEvent(currentDistance);
+        ItemEvent nextItem = currentRoad.GetNextItem(currentDistance);
+
+        if (nextStop != null && nextStop.distance <= currentDistance)
+        {
+            // do stop event
+        }
+
+        if (nextEvent != null && nextEvent.distance <= currentDistance)
+        {
+            // do special event
+        }
+
+        if (nextItem != null && nextItem.distance <= currentDistance)
+        {
+            // do next item
+        }
+
+        if(currentRoad.distance <= currentDistance)
+        {
+            // do event arrive a destination
+            print("Welcome to the amazing city of " + currentRoad.currentDestination.nom);
+        }
+    }
+
+    public void SetRoad(Road road)
+    {
+        print("Lets go boys n grills");
+
+        currentRoad = road;
+        startTime = Time.time;
+    }
+}
---
+++ PixelChallenge2017/Assets/Game/Alex/Stop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Structure de donnees pour les arrets (ville, snack, depaneur)
+public class Stop {
+
+    public stopType type;
+
+    public float distance;
+
+    public enum stopType
+    {
+        Snack = 1,
+        Essence = 2
+    }
+
+    public Stop(stopType type, float distance)
+    {
+        this.type = type;
+        this.distance = distance;
+    }
+}
