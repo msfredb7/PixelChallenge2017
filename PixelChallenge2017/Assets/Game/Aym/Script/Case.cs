6a4e0260263191ff95a4ba5f7eb3ced7a95ed570
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-04-06 23:15:15 -0400

    GrilleMaker

---
+++ PixelChallenge2017/Assets/Game/Aym/Script/Case.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Case : MonoBehaviour {
+
+
+    #region position
+    private int _posX;
+    private int _posY;
+
+    public int posX
+    {
+        get
+        {
+            return _posX;
+        }
+        set
+        {
+            _posX = value;
+        }
+    }
+
+    public int posY
+    {
+        get
+        {
+            return _posY;
+        }
+        set
+        {
+            _posY = value;
+        }
+    }
+    #endregion
+
+    #region voisins
+    public Case Haut;
+    public Case Droite;
+    public Case Gauche;
+    public Case Bas;
+    #endregion
+
+
+
+
+}
---
+++ PixelChallenge2017/Assets/Game/Aym/Script/Grille.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grille : MonoBehaviour {
+
+    public Case[][] grille;
+
+
+    #region GrilleMaker
+    public GameObject prefabCase;
+    public float distanceX;
+    public float distanceY;
+
+    public int nbCaseX;
+    public int nbCaseY;
+
+    public bool produceGrille;
+
+    private void initGrille()
+    {
+        grille = new Case[nbCaseX][];
+        for(int x = 0; x < nbCaseX;x++)
+        {
+            grille[x] = new Case[nbCaseY];
+        }
+
+        //Instantiate the grille
+        for(int x = 0;x<nbCaseX;x++)
+        {
+            for(int y = 0; y<nbCaseY;y++)
+            {
+                GameObject temp = Instantiate(prefabCase,transform);
+                temp.transform.Translate(new Vector3(x*distanceX, y * distanceY,0));
+                grille[x][y] = temp.GetComponent<Case>();
+            }
+        }
+
+        //Lie les voisins
+        for (int x = 0; x < nbCaseX; x++)
+        {
+            for (int y = 0; y < nbCaseY; y++)
+            {
+                if(x>0)
+                {
+                    grille[x][y].Gauche = grille[x - 1][y];
+                }
+                if(y>0)
+                {
+                    grille[x][y].Bas = grille[x][y - 1];
+                }
+                if(x<nbCaseX-1)
+                {
+                    grille[x][y].Droite = grille[x + 1][y];
+                }
+                if(y<nbCaseY-1)
+                {
+                    grille[x][y].Haut = grille[x][y + 1];
+                }
+            }
+        }
+
+
+    }
+
+    #endregion
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if(produceGrille)
+        {
+            initGrille();
+        }
+
+    }
+
+}
