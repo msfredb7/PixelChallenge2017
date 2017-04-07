3da0a45ce7fc5258523399330b84689fc0d8c365
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-04-07 18:27:50 -0400

    MajGrille

---
+++ PixelChallenge2017/Assets/Game/Aym/Script/Nourriture.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nourriture : Item {
+
+    public int ValeurNourriture;
+
+	// Use this for initialization
+	protected override void Start () {
+		
+	}
+
+    // Update is called once per frame
+    override protected void Update () {
+		
+	}
+
+    public override void EndDrag()
+    {
+        
+        CalculCollidedCase();
+        Case temp = calculCentralCase();
+
+        foreach(Item it in Item.allItem)
+        {
+            if(it.centralCase == temp && it.GetType() == typeof(Personne))
+            {
+                Personne p = (Personne)it;
+                p.food += ValeurNourriture;
+                Destroy(gameObject);
+                return;
+            }
+    
+        }
+        base.EndDrag();
+    }
+}
