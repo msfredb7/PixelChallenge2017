c3faf436909d4bdb251bd1d8a5ea6602d959f7dc
Xeal <alexandre-blanchet94@hotmail.com>
2017-04-07 10:57:36 -0400

    quick fix erreurs

---
+++ PixelChallenge2017/Assets/Game/Alex/Scripts/Condition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condition : MonoBehaviour {
+
+    public enum conditionType
+    {
+        Destination = 0,
+        Item = 1
+    }
+
+    public conditionType currentCondition;
+    public Ville currentVille;
+    public Item currentItem;
+
+    public Condition(Ville destination)
+    {
+        currentCondition = conditionType.Destination;
+
+    }
+}
