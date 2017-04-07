37cf6bc05450683d488d29835dfb4a8e7f91d9b5
Fred Bessette <msfredb7@gmail.com>
2017-04-07 17:49:51 -0400

    Oil !

---
+++ PixelChallenge2017/Assets/Game/Fred/OilDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OilDisplay : Singleton<OilDisplay>
+{
+    public MouseEnterListener listener;
+    public Image fill;
+    private Vector2 fullSizeDelta;
+
+    void Start()
+    {
+        fullSizeDelta = fill.rectTransform.sizeDelta;
+    }
+    public static void UpdateOil(float ratio)
+    {
+        instance.fill.rectTransform.sizeDelta = new Vector2(instance.fullSizeDelta.x, instance.fullSizeDelta.y * ratio);
+    }
+    public static bool IsMouseIn()
+    {
+        return instance.listener.isIn;
+    }
+}
