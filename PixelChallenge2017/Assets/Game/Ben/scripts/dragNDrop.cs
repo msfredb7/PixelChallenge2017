3ff86bc26c2b02179729bc879f5eba58f336973d
DESKTOP-A46VL7R\etata <fivocx@gmail.com>
2017-04-07 10:31:55 -0400

    obseletesutff

--- PixelChallenge2017/Assets/Game/Ben/scripts/dragNDrop.cs
+++ PixelChallenge2017/Assets/Game/Ben/scripts/dragNDrop.cs
@@ -4,28 +4,77 @@ using UnityEngine;
 
 public class dragNDrop : MonoBehaviour {
 
-    private bool dragging;
-    private Plane plan;
     public float depth;
+    public Item item;
+
+    private bool dragging;
+    private Vector3 objPos;
+    private bool onCase;
+    private int maxShapeSize = 7;
+
+    private void Start()
+    {
+        maxShapeSize = ArrayLayout.
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
         if (dragging)
-        {
-            Vector2 mousePos = Input.mousePosition;
-            Vector3 objPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
+        { 
+            int layer = 1 << 8;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10.0f, layer))
+            {
+                testPosition(hit);
+            }
+
+            else
+            {
+                Vector2 mousePos = Input.mousePosition;
+                objPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
+            }
             transform.position = objPos;
         }
     }
 
-    private void OnMouseDown()
+
+    public void testPosition(RaycastHit hit)
     {
-        dragging = true;
+        Case hitCase = hit.transform.GetComponent<Case>();
+
+        
+
+
+        objPos = hit.transform.position;
+        objPos.y += 0.1f;
+
     }
 
-    private void OnMouseUp()
+
+
+    public void OnMouseDown()
+    {
+        dragging = true;
+        objPos = transform.position;
+    }
+
+    public void OnMouseUp()
     {
         dragging = false;
+        transform.position = objPos;
     }
-} 
+
+
+
+  
+
+
+
+
+}
+ 
