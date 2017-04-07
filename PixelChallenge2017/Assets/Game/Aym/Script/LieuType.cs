7ceebc442361b1c182f8931fd1b884d04fceca83
Fred Bessette <msfredb7@gmail.com>
2017-04-07 14:38:15 -0400

    + arretBus

--- PixelChallenge2017/Assets/Game/Aym/Script/LieuType.cs
+++ PixelChallenge2017/Assets/Game/Aym/Script/LieuType.cs
@@ -2,4 +2,4 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum LieuType { stationEssence, laveAuto, depaneur, Montreal, Quebec, TroisRiviere, SeptIles, restaurant, garage, costco };
+public enum LieuType { stationEssence, laveAuto, depaneur, Montreal, Quebec, TroisRiviere, SeptIles, restaurant, garage, costco, arretBus };
