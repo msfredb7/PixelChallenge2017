a8f4a6eedb3382068a9f87229bbafb9b3d7c7f6e
eboucher4 <eric_2212@hotmail.ca>
2017-04-07 11:27:23 -0400

    Road1 Fix
    Road1

--- PixelChallenge2017/Assets/Game/Alex/Scripts/EventScripting.cs
+++ PixelChallenge2017/Assets/Game/Alex/Scripts/EventScripting.cs
@@ -10,6 +10,13 @@ public class EventScripting {
         Ville depart = new Ville("St-Stanislas");
         Ville destination = new Ville("Montreal");
 
+        List<Item> randomItemBank1 = new List<Item>();
+        // randomItemBank1.Add(...);
+
+        List<Item> randomItemBank2 = new List<Item>();
+        // randomItemBank2.Add(...);
+
+
         // Evenement a faire...
 
         List<Stop> stopRoad1 = new List<Stop>();
@@ -17,13 +24,7 @@ public class EventScripting {
         List<ItemAVendre> station40km = new List<ItemAVendre>();
         List<ItemAVendre> restaurant47km = new List<ItemAVendre>();
         List<ItemAVendre> garage62km = new List<ItemAVendre>();
-
-        List<Item> randomItemBank1 = new List<Item>();
-        // randomItemBank1.Add(...);
-
-        List<Item> randomItemBank2 = new List<Item>();
-        // randomItemBank2.Add(...);
-
+        
         List<ItemEvent> itemEventRoad1 = new List<ItemEvent>();
         // ItemEvent item29km = new ItemEvent(29)
         // ItemEvent item35km = new ItemEvent(35)
@@ -38,11 +39,12 @@ public class EventScripting {
         // depanneur22km.Add(new ItemAVendre, )
         // stopRoad1.Add(new Stop(22,));
 
-        QuestManager.instance.AddQuest(new Quest("Charles-Montreal"));
-        QuestManager.instance.AddQuest(new Quest("Marc-Station"));
-        QuestManager.instance.AddQuest(new Quest("Monique-Montreal"));
+        List<Quest> questRoad1 = new List<Quest>();
+        questRoad1.Add(new Quest("Charles-Montreal", 10, destination));
+        // questRoad1.Add(new Quest("Marc-Station", 17, ));
+        questRoad1.Add(new Quest("Monique-Montreal", 57, destination));
 
-        Road newRoad = new Road(depart, destination, stopRoad1, null,itemEventRoad1,70);
+        Road newRoad = new Road(depart, destination, stopRoad1, null,itemEventRoad1,questRoad1,70);
         RoadManager.instance.SetRoad(newRoad);
     }
 
@@ -54,7 +56,7 @@ public class EventScripting {
                 Ville depart = new Ville("Montreal");
                 Ville destination = new Ville("Trois-Riviere");
 
-                // Evennement a faire...
+                // Evenement a faire...
 
                 Road newRoad = new Road(depart, destination, null, null, null, 5);
                 RoadManager.instance.SetRoad(newRoad);
@@ -64,7 +66,7 @@ public class EventScripting {
                 Ville depart1 = new Ville("Trois-Riviere");
                 Ville destination1 = new Ville("Quebec");
 
-                // Evennement a faire...
+                // Evenement a faire...
 
                 Road newRoad1 = new Road(depart1, destination1, null, null, null, 5);
                 RoadManager.instance.SetRoad(newRoad1);
@@ -74,7 +76,7 @@ public class EventScripting {
                 Ville depart2 = new Ville("Quebec");
                 Ville destination2 = new Ville("Saguenay");
 
-                // Evennement a faire...
+                // Evenement a faire...
 
                 Road newRoad2 = new Road(depart2, destination2, null, null, null, 5);
                 RoadManager.instance.SetRoad(newRoad2);
@@ -84,7 +86,7 @@ public class EventScripting {
                 Ville depart3 = new Ville("Saguenay");
                 Ville destination3 = new Ville("Sept-Iles");
 
-                // Evennement a faire...
+                // Evenement a faire...
 
                 Road newRoad3 = new Road(depart3, destination3, null, null, null, 5);
                 RoadManager.instance.SetRoad(newRoad3);
