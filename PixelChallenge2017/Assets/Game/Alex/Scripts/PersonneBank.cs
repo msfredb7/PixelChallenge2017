0080fe3821f8387a9b5d99b78e5b97fe2b527b4a
Xeal <alexandre-blanchet94@hotmail.com>
2017-04-07 19:27:32 -0400

    quete et personne

---
+++ PixelChallenge2017/Assets/Game/Alex/Scripts/PersonneBank.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CCC.Manager;
+using UnityEngine;
+
+public class PersonneBank : Bank<Personne>
+{
+    public Personne[] listePersonne;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < listePersonne.Length; i++)
+        {
+            AddItem(listePersonne[i]);
+        }
+    }
+
+    public Personne GetPersonneByName(string name)
+    {
+        for (int i = 0; i < listePersonne.Length; i++)
+        {
+            if (listePersonne[i].nom == name)
+                return listePersonne[i];
+        }
+        return null;
+    }
+}
