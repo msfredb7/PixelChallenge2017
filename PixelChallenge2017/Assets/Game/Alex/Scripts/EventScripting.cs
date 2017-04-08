dc1e8703b43e8595906783694d58a7ae6a91bbb9
eboucher4 <eric_2212@hotmail.ca>
2017-04-08 12:06:10 -0400

    Blabla
    Encore du script

--- PixelChallenge2017/Assets/Game/Alex/Scripts/EventScripting.cs
+++ PixelChallenge2017/Assets/Game/Alex/Scripts/EventScripting.cs
@@ -102,7 +102,7 @@ public class EventScripting
         stopRoad1.Add(monique57km);
 
         List<Quest> questRoad1 = new List<Quest>();
-        questRoad1.Add(new Quest("Deposer Marise et ses valises a Montreal", 0, 0, null, new Quest.Destination(destination, null)));
+        questRoad1.Add(new Quest("Deposer Marise et ses valises a Montreal", 0, 0, GameManager.instance.personne, new Quest.Destination(destination, null)));
         questRoad1.Add(new Quest("Deposer Charles et son équipement de hockey a Montreal", 10, 25, PersonneBank.GetItemByIndex(0), new Quest.Destination(destination, null), questItemCharles10km));
         questRoad1.Add(new Quest("Deposer Marc à la prochaine station service", 17, 20, PersonneBank.GetItemByIndex(0), new Quest.Destination(null, station40km)));
         questRoad1.Add(new Quest("Deposer Monique a Montreal", 57, 20, PersonneBank.GetItemByIndex(0), new Quest.Destination(destination, null)));
