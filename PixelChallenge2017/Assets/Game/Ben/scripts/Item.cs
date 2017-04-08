38e1c4fcb43f0f00c28968dc43a87764cf796f69
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-04-08 01:20:22 -0400

    ToolTip for Item
    Il faudra faire la description des item
    Possibilité d'overide pour mieux décrire les personne

--- PixelChallenge2017/Assets/Game/Ben/scripts/Item.cs
+++ PixelChallenge2017/Assets/Game/Ben/scripts/Item.cs
@@ -49,6 +49,8 @@ public class Item : MonoBehaviour
     public ItemState _placementState;
     public int wasInCar =-1;
 
+    public string descriptionTxt;
+
     ItemState placementState
     {
         get
@@ -510,6 +512,24 @@ public class Item : MonoBehaviour
         //GlobalAnimator.RemoveFloatingItem(gameObject);
         //DestroyImmediate(gameObject,true);
     }
+
+
+    public virtual string description()
+    {
+        return descriptionTxt;
+    }
+
+    public void OnMouseOver()
+    {
+        Tooltip.instance.PrintToolTip(description());
+    }
+
+    public void OnMouseExit()
+    {
+        Tooltip.instance.HideToolTip();
+    }
+
+
 }
 
 
