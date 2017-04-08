9388ea165dadc28890027a1db74afacee5f18548
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-04-07 21:45:04 -0400

    QucikExceptionFix

--- PixelChallenge2017/Assets/Game/Ben/scripts/Item.cs
+++ PixelChallenge2017/Assets/Game/Ben/scripts/Item.cs
@@ -478,8 +478,8 @@ public class Item : MonoBehaviour {
     public void Kill()
     {
         allItem.Remove(this);
-        //gameObject.SetActive(false);
-        DestroyImmediate(gameObject,true);
+        gameObject.SetActive(false);
+        //DestroyImmediate(gameObject,true);
     }
 }
 
